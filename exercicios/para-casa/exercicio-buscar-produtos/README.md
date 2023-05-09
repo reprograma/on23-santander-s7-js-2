847cfa25e422341b2bdd7efec61b9d5fd2be1510
Mia Azevedo <mialuzazevedo@gmail.com>
2023-05-09 02:28:33 -0400

    Exercício buscador de produtos, versao inicial completa

---
+++ exercicios/para-casa/exercicio-buscar-produtos/README.md
@@ -0,0 +1,3 @@
+# Calculadora de desconto
+
+link do site: https://mialuzmia.github.io/on23-santander-s6-js-1/exercicios/para-casa/exercicio-desconto/index.html
---
+++ exercicios/para-sala/exercicio/ex-filmes.js
@@ -0,0 +1,72 @@
+let filmes = [
+    {
+      nome: "Deadpool",
+      classificacaoEtaria: 18 
+    },
+    {
+      nome: "Your Name",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Kill Bill",
+      classificacaoEtaria: 18
+    },
+    {
+      nome: "Shrek",
+      classificacaoEtaria: 0 
+    },
+    {
+      nome: "Matrix ",
+      classificacaoEtaria: 14 
+    },
+    {
+      nome: "Você nem imagina",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Brilho eterno de uma mente sem lembranças",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "Scott Pilgrim contra o mundo",
+      classificacaoEtaria: 12
+    },
+    {
+      nome: "Capitão Fantástico",
+      classificacaoEtaria: 14
+    },
+    {
+      nome: "El Camino",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Parasita",
+      classificacaoEtaria: 16 
+    },
+    {
+      nome: "Star Wars: o retorno do jedi",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Red: crescer é uma fera",
+      classificacaoEtaria: 0
+    },
+    {
+      nome: "Ninja Assassino",
+      classificacaoEtaria: 18
+    },
+  ]
+
+let filmesPermitidos = []
+
+function possoAssitir(idade){
+    for (let i = 0; i < filmes.length; i++){
+        if(filmes[i].classificacaoEtaria <= idade){
+            filmesPermitidos.push(filmes[i].nome)
+        }
+    }
+    let res = filmesPermitidos.join(', ')
+    console.log(`Você pode assitir: ${res}.`) 
+}
+
+possoAssitir(12)
