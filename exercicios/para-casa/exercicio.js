7a9c46afc7a4c97cf624a2d67577dff024040463
Annie Santana <annie.santtana@gmail.com>
2023-05-12 22:27:55 -0300

    correcoes no código

--- exercicios/para-casa/exercicio.js
+++ exercicios/para-casa/exercicio.js
@@ -9,92 +9,98 @@ DICA: Já deixar no html os parágrafos para preencher com as informações do p
 
 
 let produtos = [
-    {
-      nome: "TV 20 polegadas",
-      categoria: "áudio e vídeo",
-      preco: 1500
-    },
-    {
-      nome: "Mouse",
-      categoria: "informática",
-      preco: 70
-    },
-    {
-      nome: "Teclado",
-      categoria: "informática",
-      preco: 350 
-    },
-    {
-      nome: "Aparelho de som",
-      categoria: "áudio e vídeo",
-      preco: 600
-    },
-    {
-      nome: "Sanduicheira",
-      categoria: "eletrodomésticos",
-      preco: 130
-    },
-    {
-      nome: "Microondas 20L",
-      categoria: "eletrodomésticos",
-      preco: 800
-    },
-    {
-      nome: "Geladeira frost free",
-      categoria: "eletrodomésticos",
-      preco: 2100
-    },
-    {
-      nome: "Carregador de notebook Dell",
-      categoria: "informática",
-      preco: 140
-    },
-    {
-      nome: "Caixa de som JBL",
-      categoria: "áudio e vídeo",
-      preco: 230
-    },
-    {
-      nome: "Webcam",
-      categoria: "informática",
-      preco: 330
-    },
-    {
-      nome: "Fogão 4 bocas",
-      categoria: "eletrodomésticos",
-      preco: 680
-    },
-    {
-      nome: "Monitor 24 polegadas",
-      categoria: "informática",
-      preco: 900
-    },
-    {
-      nome: "Air fryer",
-      categoria: "eletrodomésticos",
-      preco: 450
+  {
+    nome: "TV 20 polegadas",
+    categoria: "áudio e vídeo",
+    preco: 1500
+  },
+  {
+    nome: "Mouse",
+    categoria: "informática",
+    preco: 70
+  },
+  {
+    nome: "Teclado",
+    categoria: "informática",
+    preco: 350
+  },
+  {
+    nome: "Aparelho de som",
+    categoria: "áudio e vídeo",
+    preco: 600
+  },
+  {
+    nome: "Sanduicheira",
+    categoria: "eletrodomésticos",
+    preco: 130
+  },
+  {
+    nome: "Microondas 20L",
+    categoria: "eletrodomésticos",
+    preco: 800
+  },
+  {
+    nome: "Geladeira frost free",
+    categoria: "eletrodomésticos",
+    preco: 2100
+  },
+  {
+    nome: "Carregador de notebook Dell",
+    categoria: "informática",
+    preco: 140
+  },
+  {
+    nome: "Caixa de som JBL",
+    categoria: "áudio e vídeo",
+    preco: 230
+  },
+  {
+    nome: "Webcam",
+    categoria: "informática",
+    preco: 330
+  },
+  {
+    nome: "Fogão 4 bocas",
+    categoria: "eletrodomésticos",
+    preco: 680
+  },
+  {
+    nome: "Monitor 24 polegadas",
+    categoria: "informática",
+    preco: 900
+  },
+  {
+    nome: "Air fryer",
+    categoria: "eletrodomésticos",
+    preco: 450
+  }
+]
+
+
+
+let prodEscolhido = document.getElementById('entrada')
+var consultar = document.querySelector("#consultar");
+
+consultar.addEventListener("click", function () {
+  console.log(prodEscolhido)
+
+  let prodEncontrado = false
+  for (let index = 0; index < produtos.length; index++) {
+
+    if (prodEscolhido.value.toUpperCase() === produtos[index].nome.toUpperCase()) {
+      let nomeProduto = document.getElementById('nomeProduto')
+      let categoria = document.getElementById('categoria')
+      let preco = document.getElementById('valor')
+
+      nomeProduto.innerHTML = produtos[index].nome;
+      categoria.innerHTML = produtos[index].categoria;
+      preco.innerHTML = produtos[index].preco;
+      prodEncontrado = true
+
     }
-  ]
-
-
-
-  let prodEscolhido = document.getElementById('entrada')
-  var consultar = document.querySelector("#consultar");
-
-  consultar.addEventListener("click", function() {
-
-    for (let index = 0; index < produtos.length; index++) {
-      console.log(index)
-        if (prodEscolhido === produtos[index].nome) {
-           let nomeProduto = document.getElementById('nomeProduto')
-           let categoria = document.getElementById('categoria')
-           let preco = document.getElementById('preco')
-
-           nomeProduto.innerHTML = produtos[index].nome;
-           categoria.innerHTML = produtos[index].categoria
-           preco.innerHTML = produtos[index].preco
-
-        } 
-      }
-    });
+  }
+  if (prodEncontrado == false) {
+    alert("Produto não encontrado")
+  }  
+});
 
